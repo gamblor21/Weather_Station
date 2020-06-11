ceb5555adcd486ad524b9fbbdc3942b6b4bea449
gamblor21 <mark.komus@gmail.com>
2020-06-10 22:07:18 -0500

    Cleanup

--- Weather_Station.ino
+++ Weather_Station.ino
@@ -1,3 +1,27 @@
+/******
+The MIT License (MIT)
+
+Copyright (c) 2020 Mark Komus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+******/
+
 #include <Adafruit_SleepyDog.h>
 #include "SparkFunBME280.h"
 #include "Wire.h"
@@ -9,11 +33,10 @@
 #define LED_PIN 5
 #define WIND_PIN 6
 #define RAIN_PIN 11
-#define WAKE_UP_PIN 10
 #define WIND_DIR_PIN A2
 
-
 BME280 bmeSensor;
+
 AdafruitIO_Feed *batVoltage = io.feed("Battery Voltage");
 AdafruitIO_Feed *temperature = io.feed("Temperature");
 AdafruitIO_Feed *humidity = io.feed("Humidity");
@@ -21,32 +44,20 @@ AdafruitIO_Feed *pressure = io.feed("Pressure");
 AdafruitIO_Feed *rain = io.feed("Rain");
 AdafruitIO_Feed *windDir = io.feed("Wind Direction");
 AdafruitIO_Feed *windSpeed = io.feed("Wind Speed");
-AdafruitIO_Feed *led = io.feed("LED");
 AdafruitIO_Feed *start = io.feed("Start");
 
 volatile unsigned int rainTicks = 0;
 volatile unsigned int windTicks = 0;
 volatile bool alarmWent = false;
-volatile bool stopSleep = false;
 
 RTCZero rtc;
 
-
-//474      uint8_t  POR:1;            /*!< bit:      0  Power On Reset                     */
-//475     uint8_t  BOD12:1;          /*!< bit:      1  Brown Out 12 Detector Reset        */
-//476     uint8_t  BOD33:1;          /*!< bit:      2  Brown Out 33 Detector Reset        */
-//477     uint8_t  :1;               /*!< bit:      3  Reserved                           */
-//478     uint8_t  EXT:1;            /*!< bit:      4  External Reset                     */
-//479     uint8_t  WDT:1;            /*!< bit:      5  Watchdog Reset                     */
-//480     uint8_t  SYST:1;           /*!< bit:      6  System Reset Request               */
-//481     uint8_t  :1;               /*!< bit:      7  Reserved                           */
-
 void setup() {
-  //delay(10000)1;
   Serial.begin(115200);
   //while (!Serial) {;}
-  Serial.println("Let's go");
-
+  
+  Serial.println("Starting up...");
+  
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // Show we're awake
   
@@ -55,19 +66,15 @@ void setup() {
 
   pinMode(WIND_PIN, INPUT_PULLUP);
   pinMode(RAIN_PIN, INPUT_PULLUP);
-  pinMode(WAKE_UP_PIN, INPUT_PULLUP);
   pinMode(WIND_DIR_PIN, INPUT);
 
   LowPower.attachInterruptWakeup(WIND_PIN, windIRQ, FALLING);
   LowPower.attachInterruptWakeup(RAIN_PIN, rainIRQ, FALLING);
-  LowPower.attachInterruptWakeup(WAKE_UP_PIN, wakeupIRQ, FALLING);
 
   rtc.begin();
   rtc.setTime(0,0,0);
   rtc.attachInterrupt(alarmIRQ);
   
-  //WiFi.maxLowPowerMode();
-
   Serial.print("Connecting to Adafruit IO");
   io.connect();
 
@@ -79,21 +86,13 @@ void setup() {
   }
 
   // we are connected
-  Serial.println();
   Serial.println(io.statusText());
 
   WiFi.lowPowerMode();
 
-  //LowPower.sleep(10000);
-
-  pinMode(LED_PIN, OUTPUT);
-  //led->onMessage(ledMessage);
-  //led->get();
-
   // Setup BME280 sensor
   bmeSensor.settings.commInterface = I2C_MODE;
   bmeSensor.settings.I2CAddress = 0x77;
-
   bmeSensor.settings.runMode = 2; // Forced mode
   bmeSensor.settings.tStandby = 0;  //  4, 500ms
   bmeSensor.settings.filter = 0; // off
@@ -103,7 +102,6 @@ void setup() {
 
   delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
   Serial.println(bmeSensor.begin(), HEX);
-
   Serial.print("ctrl_meas(0xF4): 0x");
   Serial.println(bmeSensor.readRegister(BME280_CTRL_MEAS_REG), HEX);
 
@@ -111,8 +109,6 @@ void setup() {
 
   rtc.setAlarmEpoch(rtc.getEpoch() + 9);
   rtc.enableAlarm(rtc.MATCH_HHMMSS);
-
-  Serial.println(SERIAL_PORT_USBVIRTUAL);
 
   int countdownMS = Watchdog.enable(16000);
   Serial.println(countdownMS);
@@ -123,18 +119,15 @@ unsigned long prevThirtyMillis = 0;
 volatile unsigned long alarmCount = 0;
 
 void loop() {
-  flashled(10);
-  flashled(10);
-  flashled(10);
   Serial.end();
 
   digitalWrite(LED_BUILTIN, LOW); 
+
   LowPower.sleep();
-  digitalWrite(LED_BUILTIN, HIGH); 
 
   Serial.begin(115200);
   //while (!Serial) {;}
-  Serial.println("Start");
+  Serial.println("Awakened");
 
   if (alarmWent) {
     Watchdog.reset();
@@ -142,63 +135,21 @@ void loop() {
       rtc.setAlarmEpoch(rtc.getEpoch() + 9);
       rtc.enableAlarm(rtc.MATCH_HHMMSS);
     }
+    alarmWent = false;
   }
   
   if (alarmCount > 5) {
+    digitalWrite(LED_BUILTIN, HIGH);
+
     rtc.setAlarmEpoch(rtc.getEpoch() + 8);
     rtc.enableAlarm(rtc.MATCH_HHMMSS);
     alarmCount = 0;
     
-    flashled(30);
     io.run();
-    Serial.println("Ran");
-    measure();
-    Serial.println("Measure done");
-    alarmWent = false;
-    //delay(3000);
-  }
 
-  if (stopSleep) {
-    flashled(500);
-    flashled(500);
-    Serial.println("Stop sleeping");
-    Serial.println(io.statusText());
-    Serial.println(rtc.getEpoch());
-    Serial.print(rtc.getHours()); Serial.print(rtc.getMinutes()); Serial.println(rtc.getSeconds());
-    Serial.print(rtc.getAlarmHours()); Serial.print(rtc.getAlarmMinutes()); Serial.println(rtc.getAlarmSeconds());
-    stopSleep = false;
+    measure();
   }
 }
-
-/*void loop() {
-  unsigned long currentMillis = millis();
-  
-  if ((currentMillis - prevMillis) >= 1000)
-  {
-    prevMillis = currentMillis;
-    //Serial.print(prevMillis);
-    //Serial.print(" ");
-    //Serial.println(millis());
-    io.run();
-  }
-  
-  if ((currentMillis - prevThirtyMillis) >= 30000) 
-  {
-    prevThirtyMillis = currentMillis;
-    //Serial.print("*");
-    //Serial.println(millis());
-    measure();
-  }
-
-  unsigned long delayMillis = ((unsigned long)1000)-(millis()-prevMillis);
-  delayMillis %= 1000; // if we pass the second mark it goes weird
-  
-  //Serial.print("Delay: ");
-  //Serial.print(millis());
-  //Serial.print(" ");
-  //Serial.println(delayMillis);
-  delay(delayMillis);
-}*/
 
 void measure() {
   float measuredvbat = analogRead(VBATPIN);
@@ -261,7 +212,7 @@ void measure() {
 
   io.run();
 
-  float wind = (float)windTicks / (float)30; //(30 seconds)
+  float wind = (float)windTicks / (float)60; //60 seconds)
   windTicks = 0;
   wind *= 2.4; // 2.4 km/h
   //Serial.print("Wind Speed: ");
@@ -272,20 +223,8 @@ void measure() {
   
 }
 
-void ledMessage(AdafruitIO_Data *data) {
-
-  Serial.print("received <- ");
-
-  if (data->toPinLevel() == HIGH)
-    Serial.println("HIGH");
-  else
-    Serial.println("LOW");
-
-  digitalWrite(LED_PIN, data->toPinLevel());
-}
-
 int getWindDirection() {
-  unsigned int windDir = analogRead(A2);
+  unsigned int windDir = analogRead(WIND_DIR_PIN);
   if (windDir < 74) return 113;
   if (windDir < 88) return 67;
   if (windDir < 110) return 90;
@@ -329,18 +268,7 @@ void rainIRQ() {
   rainTicks++;
 }
 
-void wakeupIRQ() {
-  stopSleep = true;
-}
-
 void alarmIRQ() {
   alarmWent = true;
   alarmCount++;
-}
-
-void flashled(int d) {
-  //digitalWrite(5, HIGH);
-  //delay(d);
-  //digitalWrite(5, LOW);
-  //delay(d);
 }
